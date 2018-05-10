3f37656d2cbca89d9ccc6d299a9eef3ba332b148
“hamzas” <skander.hamza@st.com>
2018-05-10 17:15:53 +0100

    got spinning result

--- myCasino.py
+++ myCasino.py
@@ -1,3 +1,4 @@
+import random
 
 wallet = 1000
 print ("amount in your wallet", wallet, "$")
@@ -5,3 +6,10 @@ bet = input ("type the number you wanna bet on: ")
 bet = int (bet)
 
 print ("your bet is ",bet , "$")
+
+result = random.randrange(49)
+
+print ("spinning ....")
+print ("spinning ....")
+print ("spinning ....")
+print ("<!> the ball is on the number : ", result,)
