d5193e351d883354c0516193b66744e8741a3924
“hamzas” <skander.hamza@st.com>
2018-05-14 14:50:14 +0100

    pendy: refactor guess word uning MAX_TRIALS

---
+++ tp_pendu/functions.py
@@ -0,0 +1,6 @@
+def pendy_init():
+    for item in sk_listToGuess:
+        global sk_listToGuess
+        sk_listToGuess = "*"
+    print sk_listToGuess
+
--- tp_pendu/pendy.py
+++ tp_pendu/pendy.py
@@ -1,20 +1,17 @@
-sk_listToGuess = ["*"]*8
-print sk_listToGuess
-sk_stringToDisplay= "".join(sk_listToGuess)
-print sk_stringToDisplay
+from data import *
+from functions import *
 
+sk_listToGuess = ["*"]*8
 wordToguess = "fonction"
+
 step = 0
-while step < 10:
+while step < MAX_TRIALS:
+   print ("".join(sk_listToGuess))
    trial = raw_input ("give a letter: ")
-   print ("your propositin is the letter {0}".format(trial))
-   #postion = wordToguess.find(str(trial))
    result = [pos for pos, char in enumerate(wordToguess) if char == str(trial)]
    if len(result) == 0:
       print ("letter not found")
    else:
       for position in result:
          sk_listToGuess [position] = trial
-   sk_stringToDisplay= "".join(sk_listToGuess)
-   print sk_stringToDisplay
    step += 1
