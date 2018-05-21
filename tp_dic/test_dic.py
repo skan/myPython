3b67bb4d5772cb24b75e2adbfdfeaab5c6416d0c
“hamzas” <skander.hamza@st.com>
2018-05-21 14:11:05 +0100

    dico : tdd : test_reverse

--- tp_dic/test_dic.py
+++ tp_dic/test_dic.py
@@ -11,7 +11,7 @@ class MyOutput(object):
  
             def __str__(self):
                 return "".join(self.data)
-stdout_org = sys.stdout
+stdout_origin = sys.stdout
 my_stdout = MyOutput()
 
 class dicTest(unittest.TestCase):
@@ -19,13 +19,18 @@ class dicTest(unittest.TestCase):
         self.fruits = OrganizedDico(raisin=50,poires=10,orange=20, pommes=30)
         
     def test_print(self):
+        output="orange = 20 \npoires = 10 \npommes = 30 \nraisin = 50 \n\n"
         try:
             sys.stdout = my_stdout
             print (self.fruits)
+            self.assertEquals( str(my_stdout), output)
         finally:
-            sys.stdout = stdout_org
- 
-        output ="orange = 20 \npoires = 10 \npommes = 30 \nraisin = 50 \n\n"
-        self.assertEquals( str(my_stdout), output)
+            sys.stdout = stdout_origin
+    
+    def test_reverse(self):
+        output="raisin = 50 \npommes = 30 \npoires = 10 \norange = 20 \n"
+        self.fruits.reverse()
+        self.assertEquals(self.fruits.__repr__(), output)
 
-unittest.main()
+if __name__ =='__main__':
+    unittest.main()
