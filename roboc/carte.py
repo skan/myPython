8a05f598db66bae7c38fd12d4d7585a0d54e1b5a
“hamzas” <skander.hamza@st.com>
2018-05-22 18:01:57 +0100

    roboc: display map

--- roboc/carte.py
+++ roboc/carte.py
@@ -10,6 +10,15 @@ def creer_labyrinthe_depuis_chaine(chaine):
         i+=1
     return labyrinthe_l
 
+def afficher_labyrinthe(labyrinthe_l):
+    i = 0
+    while i < len (labyrinthe_l):
+        labyrinthe_l[i]="".join(labyrinthe_l[i])
+        labyrinthe_l[i] +="\n"
+        i+=1
+    labyrinthe_c = "".join(labyrinthe_l)
+    print(labyrinthe_c)
+
 
 class Carte:
 
@@ -18,6 +27,7 @@ class Carte:
     def __init__(self, nom, chaine):
         self.nom = nom
         self.labyrinthe = creer_labyrinthe_depuis_chaine(chaine)
+        afficher_labyrinthe(self.labyrinthe)
 
     def __repr__(self):
         return "<Carte {}>".format(self.nom)
